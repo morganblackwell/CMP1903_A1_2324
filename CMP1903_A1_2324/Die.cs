785a595d946208328b5ce81c6d4393e8c59259a4
Morgan <99492170+morganblackwell@users.noreply.github.com>
2024-03-02 21:50:00 +0000

    XML comments

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -13,11 +13,13 @@ namespace CMP1903_A1_2324
          * and one method that rolls the die, returns and integer and takes no parameters.
          */
 
+        /// <summary> returns the currentValue of a randomly rolled 6 sided die. </summary>
+
         // Private variable
         private int _currentValue;
 
         // Property
-        public int currentValue
+        public int CurrentValue
         {
             get { return _currentValue; }
             set { _currentValue = value; }
@@ -29,8 +31,8 @@ namespace CMP1903_A1_2324
         // Returns a random integer between 1 and 6
         public int Roll()
         {
-            currentValue = _random.Next(1, 7); // 1 to 7 where 7 is not included
-            return currentValue;
+            CurrentValue = _random.Next(1, 7); // 1 to 7 where 7 is not included
+            return CurrentValue;
         }
     }
 }
